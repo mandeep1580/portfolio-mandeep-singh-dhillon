cdbf367ebf06878a6375e0a4553c8a10e532590c
mandeep1580 <mandeep15@gmail.com>
2020-12-15 00:42:05 -0800

    Added basic html for a card

--- my-portfolio-app/src/Components/Portfolio.js
+++ my-portfolio-app/src/Components/Portfolio.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 export default class Portfolio extends Component {
     render() {
         return (
-            <div>
-                
+            
+            <div class="container">
+                <img src="img_snow_wide.jpg" alt="Snow" style="width:100%;">
+                <div class="bottom-left">Bottom Left</div>
+                <div class="top-left">Top Left</div>
+                <div class="top-right">Top Right</div>
+                <div class="bottom-right">Bottom Right</div>
+                <div class="centered">Centered</div>
             </div>
+                
+            
         )
     }
 }
