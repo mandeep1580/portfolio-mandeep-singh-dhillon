9021c0fdee3d3b1e6d75609cb9153562d2940ce9
mandeep1580 <mandeep15@gmail.com>
2020-12-15 00:42:23 -0800

    Added basic html for Hero

---
+++ my-portfolio-app/src/Components/Hero.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+
+
+export default class Hero extends Component {
+    render() {
+        return (
+         
+                  <div class="hero-image">
+                    <div class="hero-text">
+                        <h1>I am John Doe</h1>
+                        <p>And I'm a Photographer</p>
+                        <button>Hire me</button>
+                    </div>
+                    </div>
+                )
+    }
+}
+
+
+
