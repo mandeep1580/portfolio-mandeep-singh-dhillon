60aeb7fe3d545e4a0a2d3b85ee07d03a69ec473e
mandeep1580 <mandeep15@gmail.com>
2020-12-15 00:08:23 -0800

    added container to the skills code

--- my-portfolio-app/src/Components/Skills.js
+++ my-portfolio-app/src/Components/Skills.js
@@ -4,11 +4,13 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 export default class Skills extends Component {
     render() {
         return (
+            <div className = 'container'>
             <div className= 'col-lg-8 col-8-md'>
                 <ProgressBar variant="success" now={40}/>
                 <ProgressBar variant="info" now={20} />
                 <ProgressBar variant="warning" now={60} />
                 <ProgressBar variant="danger" now={80} />
+            </div>
             </div>
         )
     }
