8b2f15114625f5de4cc0451a747f523015536408
mandeep1580 <mandeep15@gmail.com>
2020-12-17 02:41:32 -0800

    Added logo to hero, edited navigation and overview

--- my-portfolio-app/src/Components/Navigation.js
+++ my-portfolio-app/src/Components/Navigation.js
@@ -9,22 +9,20 @@ export default class Navigation extends Component {
     render() {
         return (
             
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top" >
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" fixed="top" >
             <Navbar.Brand href="#home">Mandeep Dhillon</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="ml-auto">
-              <Nav.Link href="#Overview">Overview</Nav.Link>  
+            <Nav className="ml-auto">
+                <Nav.Link href="#Overview">Overview</Nav.Link>  
                 <Nav.Link href="#Resume">Resume</Nav.Link>
                 <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
                 <Nav.Link href="#Contact">Contact</Nav.Link>
-                <Nav.Link href="#pricing">Contact</Nav.Link>
-
-
             </Nav>
             
             </Navbar.Collapse>
           </Navbar>
+       
 
         )
     }
