a1caffb1b4f6eae33ebeaba2bd787c0b94c46527
mandeep1580 <mandeep15@gmail.com>
2020-12-17 19:10:26 -0800

    adding header services to the landing page

---
+++ my-portfolio-app/src/Components/Services.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import CardGroup from 'react-bootstrap/CardGroup'
+import { BsFillAlarmFill } from "react-icons/bs";
+
+export default class services extends Component {
+    render() {
+        return (
+            <div className="container">
+               <CardGroup>
+                    <Card style={{ width: '18rem' }}>
+                    <h1><BsFillAlarmFill /></h1>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                    </Card>
+                
+                  <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                    </Card>
+                </CardGroup>
+            </div>
+        )
+    }
+}
