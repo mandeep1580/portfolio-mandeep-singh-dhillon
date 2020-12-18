65f6e10b5a731a8ea1f9278cecbc3663f908182d
mandeep1580 <mandeep15@gmail.com>
2020-12-17 19:10:00 -0800

    Added container to navbar

--- my-portfolio-app/src/Components/Navigation.js
+++ my-portfolio-app/src/Components/Navigation.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
-
-
-
 export default class Navigation extends Component {
     render() {
         return (
             
             <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" fixed="top" >
+             <div className = "container">
+             <div className="nav-section">
             <Navbar.Brand href="#home">Mandeep Dhillon</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
@@ -19,10 +18,11 @@ export default class Navigation extends Component {
                 <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
                 <Nav.Link href="#Contact">Contact</Nav.Link>
             </Nav>
-            
             </Navbar.Collapse>
-          </Navbar>
-       
+            </div>
+            </div>
+            </Navbar>
+          
 
         )
     }
