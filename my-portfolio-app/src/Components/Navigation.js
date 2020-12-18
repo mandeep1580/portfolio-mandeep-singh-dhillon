a4ac46ba48cea06c0d741659126271b1d0dc7e67
mandeep1580 <mandeep15@gmail.com>
2020-12-18 02:05:58 -0800

    Applied routing and linked pages to navigation

--- my-portfolio-app/src/Components/Navigation.js
+++ my-portfolio-app/src/Components/Navigation.js
@@ -6,21 +6,17 @@ export default class Navigation extends Component {
     render() {
         return (
             
-            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" fixed="top" >
-             <div className = "container">
-             <div className="nav-section">
-            <Navbar.Brand href="#home">Mandeep Dhillon</Navbar.Brand>
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                    <Navbar.Brand href="/">Mandeep Dhillon</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto">
-                <Nav.Link href="#Overview">Overview</Nav.Link>  
-                <Nav.Link href="#Resume">Resume</Nav.Link>
-                <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
-                <Nav.Link href="#Contact">Contact</Nav.Link>
+                <Nav.Link href= "/overview">Overview</Nav.Link>
+                <Nav.Link href="/portfolio">Portfolio</Nav.Link>  
+                <Nav.Link href="/resume">Resume</Nav.Link>
+                <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav>
             </Navbar.Collapse>
-            </div>
-            </div>
             </Navbar>
           
 
