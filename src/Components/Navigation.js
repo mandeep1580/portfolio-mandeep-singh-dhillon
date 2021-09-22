b47e8f41791a58a6fcbc87fa81e82386f4fcbda0
mandeep1580 <mandeep15@gmail.com>
2021-09-22 12:10:19 -0700

    changed navigation to portfolio2

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -7,18 +7,20 @@ export default class Navigation extends Component {
         return (
 
             <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" sticky="top">
-                    <Navbar.Brand href="/">Mandeep Dhillon</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto">
-                <Nav.Link href= "/about">About Me</Nav.Link>
-                <Nav.Link href="/portfolio">Portfolio</Nav.Link>  
-                <Nav.Link href="/experience">Experience</Nav.Link>
-                <Nav.Link href="/contact">Contact</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
+                <Navbar.Brand href="/">Mandeep Dhillon</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/about">About Me</Nav.Link>
+                        {/* <Nav.Link href="/portfolio">Portfolio</Nav.Link>   */}
+                        <Nav.Link href="/portfolio2">Portfolio</Nav.Link>
+                        <Nav.Link href="/experience">Experience</Nav.Link>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
+
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
-          
+
 
         )
     }
