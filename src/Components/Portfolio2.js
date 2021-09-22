6b83537d199ac7fdcfec6fedda46358af6b25e42
mandeep1580 <mandeep15@gmail.com>
2021-09-22 12:10:55 -0700

    deigned laytout for portflio2

--- src/Components/Portfolio2.js
+++ src/Components/Portfolio2.js
@@ -1,45 +1,65 @@
 import React, { Component } from 'react'
 import Websites from './Websites'
-import Row from 'react-bootstrap/Row'
+import { Row } from 'react-bootstrap'
 
 
-export default class Portfolio extends Component {
+export default class portfolio2 extends Component {
     render() {
         return (
-            
-            <div className="container">
-            <hr></hr>
-            <div className="heading">    
-            <h1>Portfolio</h1>
-            </div>
-            <Row>
-            <div className="row my-4">
-                <Websites 
-                img= "images/parlez.png"
-                name= "Parlez"
-                tech= "React and DotNet"
-                />
-                <Websites 
-                img= "images/walkabooks.png"
-                name= "Walkabooks"
-                tech= "Angular"
-                />
-                <Websites 
-                img= "images/bc-tourism.png"
-                name= "BC Tourism"
-                tech= "HTML and CSS"
-                />
-                <Websites 
-                img= "images/tmdb.png"
-                name= "The Movie Database"
-                tech= "React"
-                />
+            <div className="content-container">
+                <div className="container">
+                    <div className="heading">
+                        <h1>Portfolio</h1>
+                    </div>
+                    <Row className="portfolio">
+                        <Websites
+                            img="images/parlez.png"
+                            name="Parlez"
+                            tech="React"
+                            website="https://parlezz.netlify.app"
+                            github="https://github.com/mandeep1580/cookit-mandeep1580"
+                        />
+                        <Websites
+                            img="images/parlez.png"
+                            name="Parlez"
+                            tech="Angular"
+                            website="https://parlezz.netlify.app"
+                            github="https://github.com/mandeep1580/cookit-mandeep1580"
+                        />
+                        <Websites
+                            img="images/parlez.png"
+                            name="Parlez"
+                            tech="Fullstack"
+                            website="https://parlezz.netlify.app"
+                            github="https://github.com/mandeep1580/cookit-mandeep1580"
+                        />
+                        <Websites
+                            img="images/parlez.png"
+                            name="Parlez"
+                            tech="Serverless"
+                            website="https://parlezz.netlify.app"
+                            github="https://github.com/mandeep1580/cookit-mandeep1580"
+                        />
+                        <Websites
+                            img="images/parlez.png"
+                            name="Parlez"
+                            tech="Kotlin"
+                            website="https://parlezz.netlify.app"
+                            github="https://github.com/mandeep1580/cookit-mandeep1580"
+                        />
+                        <Websites
+                            img="images/parlez.png"
+                            name="Parlez"
+                            tech="Swift"
+                            website="https://parlezz.netlify.app"
+                            github="https://github.com/mandeep1580/cookit-mandeep1580"
+                        />
+                    </Row>
+
                 </div>
-                </Row>
-                        
+
             </div>
-                
-            
+
         )
     }
 }
