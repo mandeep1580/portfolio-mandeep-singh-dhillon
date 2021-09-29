de79f7a2e1b63f5e89720f98f8f992609f74b7c3
mandeep1580 <mandeep15@gmail.com>
2021-09-29 12:00:58 -0700

    refactored  the portflio code

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import CardColumns from 'react-bootstrap/CardColumns'
-import Card from 'react-bootstrap/Card'
+import Websites from './Websites'
+import { Row } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
 import Link from "react-bootstrap/NavLink"
 
 
@@ -9,88 +10,56 @@ export default class Portfolio extends Component {
         return (
             <div className="content-container">
                 <div className="container">
-                <div className="heading">    
-                <h1>Portfolio</h1>
-                <hr></hr>
-                </div>
-                <div className="portfolio">              
-                <CardColumns>
-                    <Card>
-                        <Card.Img variant="top" src= "images/parlez.png" />
-                        <Card.Body>
-                        <Link href="https://parlezz.netlify.app">
-                        <Card.Title>Parlez</Card.Title>
-                        </Link>
-                        <Card.Text>
-                            A chat room app made with <strong>React and DotNet</strong>
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="images/tmdb.png"/>
-                        <Card.Body>
-                        <Link href="">
-                        <Card.Title>The Movie App</Card.Title>
-                        </Link>
-                        <Card.Text>
-                            An api based movie app made with <strong>React</strong>
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                        
-                    <Card>
-                        <Card.Img variant="top" src="images/walkabooks.png"/>
-                        <Card.Body>
-                        <Link href="https://walkabooks.netlify.app">
-                        <Card.Title>Walkabooks</Card.Title>
-                        </Link>
-                        <Card.Text>
-                            An ecommerce app made with <strong>Angular</strong>
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <div className="heading">
+                        <h1>Portfolio</h1>
+                    </div>
+                    <Row className="portfolio">
+                        <Websites
+                            img="images/tmdb.png"
+                            name="The Movie App"
+                            tech=" Api based app - React"
+                            website="https://determined-mcclintock-b7a166.netlify.app/"
+                            github="https://github.com/mandeep1580/my-movie-app"
+                        />
+                        <Websites
+                            img="images/walkabooks.png"
+                            name="Walkabooks"
+                            tech="Front End - Angular"
+                            website="https://walkabooks.netlify.app"
+                            github="https://github.com/mandeep1580/front-end-project-group6-front-end-project.git"
+                        />
+                        <Websites
+                            img="images/nomadic-compass.png"
+                            name="Nomadic Compass"
+                            tech="Fullstack - MERN"
+                            website="https://nervous-montalcini-fcf9b8.netlify.app/"
+                            github="https://github.com/mandeep1580/nomadic-compass.git"
+                        />
+                        <Websites
+                            img="images/parlez.png"
+                            name="Parlez"
+                            tech="Fullstack app - React and .Net"
+                            website="https://parlezz.netlify.app"
+                            github="https://github.com/mandeep1580/dotnetproject-team1_netproject"
+                        />
+                        <Websites
+                            img="images/jot-it-down.png"
+                            name="Jot It Down"
+                            tech="Fullstack - Serverless"
+                            website=""
+                            github="https://github.com/mandeep1580/front-end-jotitdown.git"
+                        />
+                    </Row>
 
-                    <Card bg="success" text="white" className="text-center p-3">
-                        <blockquote className="blockquote mb-0 card-body">
-                        <p>
-                            Intellectual growth should commence at birth and cease only at death.
-                        </p>
-                        <footer className="blockquote-footer">
-                            <small className="text-muted">
-                            Albert Einstine
-                            </small>
-                        </footer>
-                        </blockquote>
-                    </Card>
-                    
-                    <Card>
-                        <Card.Img src="images/bc-tourism.png" />
-                        <Card.Body>
-                        <Link href="https://bc-tourism.netlify.app">
-                        <Card.Title>BC Tourism</Card.Title>
-                        </Link>
-                        <Card.Text>
-                            An information app made with <strong>HTML and CSS</strong>
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
+                </div>
+                <div className="button">
+                    <Link href="/experience">
+                        <Button variant="primary">Portfolio</Button>
+                    </Link>
+                </div>
 
-                    <Card>
-                    <Card.Img src="images/adventurescapes.png" />
-                        <Card.Body>
-                        <Link href="https://adventurescapes.netlify.app/">
-                        <Card.Title>Adventure Escapes</Card.Title>
-                        </Link>
-                        <Card.Text>
-                            An adventure tourism app
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    </CardColumns>
-                </div>
-                </div>
             </div>
-            
+
         )
     }
 }
