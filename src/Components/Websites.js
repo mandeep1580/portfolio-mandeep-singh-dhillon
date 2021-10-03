058519d636fc1311b0785a98ceced9afb0e9366d
mandeep1580 <mandeep15@gmail.com>
2021-10-02 20:08:35 -0700

    added class to proj-image

--- src/Components/Websites.js
+++ src/Components/Websites.js
@@ -10,7 +10,7 @@ export default class Websites extends Component {
     render() {
         return (
             <Card className="websites col-lg-3 col-md-6 col-sm-12">
-                <img src={this.props.img} alt="" />
+                <img className= "proj-image " src={this.props.img} alt="" />
                 <h5 className="text-muted mt-3">{this.props.name}</h5>
                 <p className="text muted mt-1">{this.props.tech}</p>
                 <Row className="button-card ">
